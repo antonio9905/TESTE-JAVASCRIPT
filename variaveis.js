64f1e739f24bfcf2a8fd0d709195bffc9cf1c6da
Antônio Brito <121908110+antonio9905@users.noreply.github.com>
2023-03-14 15:04:08 -0300

    arquivo de variáveis

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+//Declarada variáveis nome_completo
+/*
+declarando mais de uma variável
+*/
+
+//--------------------
+/*
+declarando variáveis com var, let e const
+*/
+var nome_completo = "josé de brito"; //variável pode ser alterada
+let idade = 46; //variável pode ser alterada
+const rg = "102023"; //variável não pode ser alterada
+
+// console.log serve para "printar/mostrar" o valor no console do navegador
+console.log(nome_completo, idade, rg)
+
+// alterar valor da variáveis
+nome_completo = "paty flor";
+idade = 32;
+
+console.log(nome_completo, idade, rg)
