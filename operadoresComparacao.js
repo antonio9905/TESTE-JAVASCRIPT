4883670e5be6c734c11e56678197d0669e2ae110
Antônio Brito <121908110+antonio9905@users.noreply.github.com>
2023-03-15 13:42:49 -0300

    pronto

---
+++ operadoresComparacao.js
@@ -0,0 +1,11 @@
+let numero = 20;
+
+const maiorQue = numero > 20; // false
+const maiorIgual = numero >= 20; // true
+const menorQue = numero < 20; // false
+const menorIgual = numero <= 20 // true
+const iqual = numero == 20; // true
+const diferente = numero !=20; // false
+
+const estritamenteIgual = numero === "20"; //false
+const estritamenteDiferente = numero !== "20"; //false
