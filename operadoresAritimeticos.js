17d369779cdc98359e4e2fea18cc62eb25eb0841
Antônio Brito <121908110+antonio9905@users.noreply.github.com>
2023-03-15 12:11:48 -0300

    alterado

---
+++ operadoresAritimeticos.js
@@ -0,0 +1,11 @@
+let numero = 20;
+
+numero = numero + 5;
+numero = numero - 10;
+numero = numero * 5;
+numero = numero / 5;
+
+// retorna o resto da divisao
+const restoDaDivisao = numero % 2
+
+console.log(restoDaDivisao)
