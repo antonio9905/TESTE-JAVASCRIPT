b3e45025a5d444641c891c9835ec6a0502c67b49
Antônio Brito <121908110+antonio9905@users.noreply.github.com>
2023-03-15 14:21:06 -0300

    operadores logicos

---
+++ operadoresLogicos.js
@@ -0,0 +1,9 @@
+const resultado1 = (true === true) && (true === false; // false
+const resultado2 = (true === true) || (true === false; // true
+const resultado3 = !(true === true);// false
+const resultado4 = !(true !=== true);// false
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
