6a6d5ddf138e0f8556bb3d95ef2e3088d339d760
Antônio Brito <121908110+antonio9905@users.noreply.github.com>
2023-03-15 11:40:26 -0300

    alterada

---
+++ operadoresAtribuicao.js
@@ -0,0 +1,12 @@
+var numero = 20;
+
+// atribuindo e calculando o valor
+numero += 10;
+numero -=5.5;
+numero *= 5;
+numero /= 25;
+
+// outra forma de atribuir e calcular
+numero = numero + 0.1;
+
+console.log(numero)
